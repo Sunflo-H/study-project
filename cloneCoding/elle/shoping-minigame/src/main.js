c345642142e641f6a0aba2ced143a7a9175bc73b
Sunflo-H <sunflo0524@gmail.com>
2021-11-14 19:46:34 +0900

    minigame complete

--- cloneCoding/elle/shoping-minigame/src/main.js
+++ cloneCoding/elle/shoping-minigame/src/main.js
@@ -2,14 +2,51 @@
 function loadItems() {
     return fetch('data/data.json')
     .then(response => response.json())
-    .then(json => console.log(json));
+    .then(json => json.items);
+}
+
+function displayItems(items){
+    const container = document.querySelector('.items');
+    container.innerHTML = items.map(item => createHTMLString(item)).join('');
+}
+
+function createHTMLString(item){
+    return `
+    <li class="shopping-list">
+        <img src="${item.image}" alt="${item.type}">
+        <span class="item_description">${item.gender}, ${item.size} size</span>
+    </li>
+    `
+}
+
+function setEventListeners(items){
+    const logo = document.querySelector('.logo');
+    const filters = document.querySelector('.filter');
+    logo.addEventListener('click',()=>displayItems(items));
+    filters.addEventListener('click',event=>{
+        onButtonClick(event,items);
+    });
+    // onButtonClick(event,items));
+}
+
+function onButtonClick(event , items){
+    if(event.target.dataset.key=='color'){
+        let color = event.target.dataset.value;
+        let filtered_items = items.filter(item=>item.color===color);
+        displayItems(filtered_items);
+    }
+    else if(event.target.dataset.key=='type') {
+        let type = event.target.dataset.value;
+        let filtered_items = items.filter(item=>item.type===type);
+        displayItems(filtered_items);
+    }
+    // items.filter(item => )
 }
 
 // main
 loadItems()
-.then(items=>{
-    // console.log('success');
-    // displayItems(items);
-    // setEventListeners(items);
-})
-.catch(console.log);
+    .then(items => {
+        displayItems(items);
+        setEventListeners(items);
+    })
+    .catch(console.log());    
