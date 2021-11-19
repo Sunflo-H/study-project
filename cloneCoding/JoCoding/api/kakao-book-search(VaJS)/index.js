8c20e5e7b528ec9c3e029bf59b5ed32a44006a24
Sunflo-H <sunflo0524@gmail.com>
2021-11-19 19:51:52 +0900

    kakao api study

--- cloneCoding/JoCoding/api/kakao-book-search(VaJS)/index.js
+++ cloneCoding/JoCoding/api/kakao-book-search(VaJS)/index.js
@@ -1,9 +1,34 @@
-const kakaoBookURL = "https://dapi.kakao.com/v3/search/book?target=title"+'&query=1';
-const option = {
-  headers: {
-    Authorization: "KakaoAK bbbe7d040459540b61c1f31e91280f78"
+function getBooks() {
+  const kakaoBookURL = "https://dapi.kakao.com/v3/search/book?target=title" + '&query=사랑';
+  const option = {
+    headers: {
+      Authorization: "KakaoAK bbbe7d040459540b61c1f31e91280f78"
+    }
   }
+  let books = fetch(kakaoBookURL, option)
+    .then(response => response.json())
+    .then(json => json.documents);
+  return books;
 }
-fetch(kakaoBookURL,option)
-  .then(response => response.json())
-  .then(json => console.log(json));
+
+function displayBooks(books) {
+  const main = document.querySelector('main');
+  const ul = main.querySelector('.books');
+  let htmlString = books.map(book=>{
+      let string = `<li class="book">
+                      <h1>${book.title}</h1>
+                      <img src=${book.thumbnail} alt=${book.title}></img>
+                    </li>
+                    `
+      console.log(string);
+      return string;
+  }).join('');
+  
+  ul.innerHTML=htmlString;
+}
+
+getBooks()
+  .then(books => {
+    displayBooks(books);
+  }
+  )
