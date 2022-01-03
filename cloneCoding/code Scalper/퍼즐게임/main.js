8c14b145a943ed45da983ca005eece182eb678a3
sunflo-h <sunflo0524@gmail.com>
2022-01-03 23:48:24 +0900

    puzzle 완성

--- cloneCoding/code Scalper/퍼즐게임/main.js
+++ cloneCoding/code Scalper/퍼즐게임/main.js
@@ -1,7 +1,7 @@
 const container = document.querySelector('.image-container');
 const startButton = document.querySelector('.start-button');
 const gameText = document.querySelector('.game-text');
-const playTime = document.querySelector('.play-tiem');
+const playTime = document.querySelector('.play-time');
 
 const TILE_COUNT = 16;
 
@@ -11,18 +11,27 @@ const dragged = {
     class:null,
     index:null,
 }
+let isPlaying = false;
+let timeInterval = null;
+let time = 0;
 
-
-setGame();
 // 모든 타일의 인덱스와 해당 data-index 값이 같으면 게임 종료
 function setGame(){
+    isPlaying = true;
     container.innerHTML="";
+    gameText.style.display = "none";
+    clearInterval(timeInterval);
+   
     tiles = createImageTiles();
     tiles.forEach(tile=>container.appendChild(tile));
     setTimeout(()=>{
         container.innerHTML="";
         shuffle(tiles).forEach(tile => container.appendChild(tile));
-    },000);
+        timeInterval = setInterval(()=>{
+            playTime.innerText = time;
+            time ++;
+        },1000);
+    },2000);
 }
 
 
@@ -49,17 +58,60 @@ function shuffle(array){
     }
     return array
 }
+// 드롭 한뒤 현재의 이미지들이 가지고 있는 인덱스를 구하고
+// 이미지가 가지고 있는 인덱스와 filter의 인덱스(=원래순서대로의 인덱스)를 비교하여
+// 다른것들만 unMatchedList에 저장한다. 
+// unMatchedList의 길이가 0이되면 게임 성공
+function checkStatus(){
+    
+    const currentList = [...container.children];
+    const unMatchedList = currentList.filter((child,index)=>{
+        // console.log(Number(child.getAttribute("data-index")));
+        return Number(child.getAttribute("data-index")) !== index
+    })
+    if(unMatchedList.length ===0) {
+        //game finish
+        gameText.style.display="block";
+        isPlaying = false;
+        
+    }
+}
 
 // event
 container.addEventListener('dragstart', e=>{
+    if(!isPlaying) return;
     const obj = e.target;
+    const obj_brothers = obj.parentNode.children;
     dragged.el = obj;
     dragged.class = obj.className;
-    dragged.index = [...obj.parentNode.children].indexOf(obj)
+    dragged.index = [...obj_brothers].indexOf(obj)
 })
 container.addEventListener('dragover', e=>{
     e.preventDefault();
 })
 container.addEventListener('drop', e=>{
-    console.log('drop');
+    const obj = e.target;
+    const obj_brothers = obj.parentNode.children;
+
+    
+    if(obj.className !== dragged.class){
+        let originPlace;
+        let isLast = false;
+
+        if(dragged.el.nextSibling){ //nextSibling이 있다면 == 마지막번째엘리멘트가 아니면
+            originPlace = dragged.el.nextSibling;
+        } else {
+            originPlace = dragged.el.previousSibling;
+            isLast = true;
+        }
+        const droppedIndex = [...obj_brothers].indexOf(obj);
+        // 드래그 중인 인덱스가 드롭존의 인덱스보다 크면 before(앞) 작으면 after(뒤)에 넣는다.
+        dragged.index > droppedIndex ? obj.before(dragged.el) : obj.after(dragged.el);
+        isLast ? originPlace.after(obj) : originPlace.before(obj);
+    }
+    checkStatus();
+})
+
+startButton.addEventListener('click',e=>{
+    setGame();
 })
